c790cfb952ccb5bf4957ef78739d702ce6d5007d
andreox <andreozzi.alessio@gmail.com>
2022-03-16 11:51:55 +0100

    modified some fields size

--- tickets/models.py
+++ tickets/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Tickets(models.Model):
     id = models.AutoField(primary_key=True)
-    head = models.CharField(max_length=100, blank=True, default='')
-    body = models.CharField(max_length=300, blank=True, default='')
-    ticket_class = models.CharField(max_length=50, blank=True, default='')
+    head = models.CharField(max_length=100000, blank=True, default='')
+    body = models.CharField(max_length=1000000, blank=True, default='')
+    ticket_class = models.CharField(max_length=500, blank=True, default='')
     
 
     class Meta:
